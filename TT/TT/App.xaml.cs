f35522edfa0a5ae82f16386b16d4edc0d3085356
marco <marco@DESKTOP-PRJ8VU9>
2023-05-02 20:10:44 +0200

    Aggiungere i file di progetto.

---
+++ TT/TT/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using TT.Services;
+using TT.Views;
+
+namespace TT
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
---
+++ TT/TT/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using TT.ViewModels;
+
+namespace TT.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
